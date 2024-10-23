e51c5cc99cdbd3170949f856576a066a347c98d8
luischapid <luischapid03@gmail.com>
2024-10-22 23:36:50 -0500

    cambios

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -3,20 +3,11 @@
       <div class="imgHeader">
         <img :src="logoSeus" alt="imagen" class="headerImage">
       </div>
-      <div class="userPerfilIcono">
-        <img :src="UserPerfil" alt="iconoPerfil">
-      </div>
     </header>
 </template>
 
 <script setup>
 import logoSeus from "../assets/img/header/logoSeus.svg"
-import UserPerfil from "./pruebas/reutilizables/UserPerfil.vue";
-
-import { onMounted } from 'vue';
-onMounted(() => {
-  console.log("Componente montado. Imagen de usuario:", UserPerfil);
-});
 
 </script>
 
@@ -24,7 +15,7 @@ onMounted(() => {
 header{
   border-radius: 0 0 20px 20px;
   display: flex;
-  justify-content: space-between;
+  justify-content: left;
   align-items: center;
   background: linear-gradient(175deg, #761D74 0%, #0DC6DE 100%);
   height: 120px;
